1aca2e6d0210e5ba724b14af5a5d6ba99d8032a6
dan <dany.condurari@gmail.com>
2023-05-15 16:17:13 +0300

    create custom btn and adjust to the size

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -12,7 +12,7 @@ interface CardProps {
 }
 
 export const Card: React.FC<CardProps> = ({ data, onDeletePost }) => {
-  const lengthDescription = data.description?.length === 100 ? "" : "...";
+  const lengthDescription = data.description?.length > 100 ? "..." : "";
 
   return (
     <div className="card">
